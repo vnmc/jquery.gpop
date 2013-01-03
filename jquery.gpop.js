9347200b0fdcab29e7c431080debd35e9ab53a11
Matthias Christen <christen@vnmc28.local>
2013-01-03 16:06:37 +0100

    added handling of right/lower borders when 'limitedToWrapper'

--- jquery.gpop.js
+++ jquery.gpop.js
@@ -22,20 +22,25 @@
 
                 },
                 showHover: function($image){
-                    $root
+                	// render the popup
+                	$root
                         .removeClass('gi-clonedImageInvisible')
                         .addClass('gi-clonedImageVisible')
-                        .css('left', $image.data('offsetL'))
-                        .css('top', $image.data('offsetT'))
+                		.find('img').attr('src', $image.attr('src'))
                         .css('max-width', $image.data('w1'))
-                        .find('img').attr('src', $image.attr('src'))
                         .css('width', $image.data('w1'))
                         .css('height', $image.data('h1'));
-
+                        
                     if($image.parent().is('a')){
                         $link.attr('href', $image.parent().attr('href'));
                     }
                     $text.html(options.renderDescription($image));
+
+					// position the popup
+                	var offs = findOffset($image);
+                    $root
+                        .css('left', offs.left)
+                        .css('top', offs.top);
                 },
                 delay: 0
             }, 
@@ -56,56 +61,25 @@
         }
         
         function init(){
-
-            var refPos = $wrapper.find('ul').position();
-            
-
             $wrapper.find('li img').each(function(index, item){
                 var w0 = $(this).width();
                 var w1 = w0 * options.scale;
                 var h0 = $(this).height();
                 var h1 = h0 * options.scale;
-                var offsetL = $(this).position().left;
-                var offsetT = $(this).position().top;
-                
-                if(options.centered){
-                    offsetL -= (w1-w0) / 2;
-                    offsetT -= (h1-h0) / 2;
-                }
-                
-                
-                if(offsetL < refPos.left){
-                    offsetL = refPos.left;
-                }
-                
-                if(offsetT < refPos.top){
-                    offsetT = refPos.top;
-                }   
-
-                           
-
-
-                
-
-                if(options.limitedToWrapper){
-                    
-                }
                 $(this)
                     .data('h0', h0)
                     .data('h1', h1)
                     .data('w0', w0)
                     .data('w1', w1)
-                    .data('offsetL', offsetL)
-                    .data('offsetT',  offsetT);
-                    
+                    .data('offsetL', null)
+                    .data('offsetT', null);
             });
-            
             
             $root.on('mouseleave.gpop', function(event){
                 $root.removeClass('gi-clonedImageVisible').addClass('gi-clonedImageInvisible');
             }); 
                         
-            $(document).on('mouseenter.gpop', '.gi-gallery ul li a img', function(event){
+            $wrapper.on('mouseenter.gpop', 'ul li a img', function(event){
                 if(delayTimer != null){
                     window.clearTimeout(delayTimer);
                 }
@@ -118,11 +92,69 @@
             $(window).on('resize.gpop', function(event){
                 if(resizeTimer != null){
                     window.clearTimeout(resizeTimer);
+                    // invalidate precalculated offsets
+                    $wrapper.find('ul li img').each(function(index, item){
+                    	$(this).data('offsetL', null).data('offsetT', null);
+                    });
                 }
                 resizeTimer = window.setTimeout(init, options.resizeTimeout);
             });
-            
         } 
+        
+        function findOffset($img){
+        	var offsetL = $img.data('offsetL');
+        	var offsetT = $img.data('offsetT');
+        	
+        	if(offsetL === null || offsetT === null)
+			{        	
+				// offsets haven't been set yet or have been invalidated: calculate
+				var w0 = $img.data('w0');	// do we really want to store these values?
+				var w1 = $root.outerWidth(true);
+				var h0 = $img.data('h0');
+				var h1 = $root.outerHeight(true);
+				offsetL = $img.position().left;
+				offsetT = $img.position().top;
+						
+				if(options.centered){
+					offsetL -= (w1-w0) / 2;
+					offsetT -= (h1-h0) / 2;
+				}
+				
+				if(options.limitedToWrapper){
+					var maxRight = 0;
+					var maxBottom = 0;
+					//var offsetWrapper = $wrapper.offset();
+					$wrapper.find('ul li img').each(function(index, item){
+						var offs = $(this).offset();
+						maxRight = Math.max(maxRight, offs.left + $(this).width());
+						maxBottom = Math.max(maxBottom, offs.top + $(this).height());
+					});
+	
+					if(offsetL + w1 > maxRight){
+						offsetL = maxRight - w1;
+					}
+					
+					if(offsetT + h1 > maxBottom){
+						offsetT = maxBottom - h1;
+					}
+				}
+								
+				var refPos = $wrapper.find('ul').position();
+
+				if(offsetL < refPos.left){
+					offsetL = refPos.left;
+				}
+				
+				if(offsetT < refPos.top){
+					offsetT = refPos.top;
+				}
+				
+				// set offset data
+				$img.data('offsetL', offsetL).data('offsetT', offsetT);
+			}
+			
+			return {left: offsetL, top: offsetT};
+        }
 
         create();
         init();
